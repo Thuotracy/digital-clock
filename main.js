08be6705a103896ee003979ed206e8f3c6691fe6
Thuotracy <tracyjacobs775@gmail.com>
2022-08-09 17:41:02 +0300

    changed the background color

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ setInterval(() =>{
     let hh = document.getElementById('hh');
     let mm = document.getElementById('mm');
     let ss = document.getElementById('ss');
+
+    let hr_dot = document.querySelector('.hr_dot');
+    let min_dot = document.querySelector('.min_dot');
+    let sec_dot = document.querySelector('.sec_dot');
+
     
     let h = new Date().getHours();
     let m = new Date().getMinutes();
@@ -26,9 +31,9 @@ setInterval(() =>{
     s = (s < 10)? "0" + s : s;
     
     
-    hours.innerHTML = h;
-    minutes.innerHTML = m;
-    seconds.innerHTML = s;
+    hours.innerHTML = h + "<br><span>Hours</span>";
+    minutes.innerHTML = m + "<br><span>Minutes</span>";
+    seconds.innerHTML = s + "<br><span>Seconds</span>";
     ampm.innerHTML = am
 
     hh.style.strokeDashoffset = 440 -(440 * h) / 12;
@@ -40,6 +45,15 @@ setInterval(() =>{
     ss.style.strokeDashoffset = 440 -(440 * s) / 60;
     // 60 seconds
 
+
+    hr_dot.style.transform = `rotate(${h * 30}deg)` ;
+    // 360 / 12 = 30
+
+    min_dot.style.transform = `rotate(${m * 6}deg)` ;
+    // 360 / 60 = 6
+
+    sec_dot.style.transform = `rotate(${s * 6}deg)` ;
+    // 360 / 60 = 6
       
 })
 
