4ad66cdcad98b0b7b6778d4c27c493556e26bf4f
Thuotracy <tracyjacobs775@gmail.com>
2022-08-09 16:06:02 +0300

    created strokeDashoffset for the hours, minutes and seconds in main.js

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ setInterval(() =>{
     let minutes = document.getElementById('minutes');
     let seconds = document.getElementById('seconds');
     let ampm = document.getElementById('ampm');
+
+    let hh = document.getElementById('hh');
+    let mm = document.getElementById('mm');
+    let ss = document.getElementById('ss');
     
     let h = new Date().getHours();
     let m = new Date().getMinutes();
@@ -26,6 +30,16 @@ setInterval(() =>{
     minutes.innerHTML = m;
     seconds.innerHTML = s;
     ampm.innerHTML = am
+
+    hh.style.strokeDashoffset = 440 -(440 * h) / 12;
+    
+    // 12 hrs clock
+    mm.style.strokeDashoffset = 440 -(440 * m) / 60;
+    
+    // 60 minutes
+    ss.style.strokeDashoffset = 440 -(440 * s) / 60;
+    // 60 seconds
+
       
 })
 
