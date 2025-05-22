a8d6be953d4c2e87dd99e4b2ea1cf175d76c09f8
Tracy Thuo <tracyjacobs775@gmail.com>
2025-05-22 21:53:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// setInterval(() =>{
+setInterval(() =>{
     let hours = document.getElementById('hours');
     let minutes = document.getElementById('minutes');
     let seconds = document.getElementById('seconds');
